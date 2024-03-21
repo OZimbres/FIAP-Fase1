851f748ad61edcf44cf8b0b25eea915fe88d89d7
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-03-21 17:23:52 -0300

    tentando python

--- Capitulos/Capitulo7/Atividade/AtividadeCapitulo7.py
+++ Capitulos/Capitulo7/Atividade/AtividadeCapitulo7.py
@@ -1,12 +1,38 @@
-anos_fumando = float(input("Há quantos anos que vossa mercê fuma? "))
-preco_maco_cigarro = float(input("Qual o valor do maço de cigarro que vossa mercê compra? "))
-macos_por_dia = float(input("Quantos maços de cigarro vossa mercê fuma por dia? "))
+anos_fumando = float(input("Há quantos anos que Vossa Mercê fuma? "))
 
-total_gasto = anos_fumando * 365 * macos_por_dia * preco_maco_cigarro
+if anos_fumando > 0:
+    macos_por_dia = float(input("Quantos maços de cigarro Vossa Mercê fuma por dia? "))
 
-if total_gasto < 20000:
-    print(f"Com o valor R$ {total_gasto:.2f}, você poderia ter dado entrada em um carro.")
-elif total_gasto < 50000:
-    print(f"Com o valor R$ {total_gasto:.2f}, você poderia ter comprado um carro popular usado")
+    if macos_por_dia > 0:
+        preco_maco_cigarro = float(input("Qual o valor do maço de cigarro que Vossa Mercê compra? "))
+
+        if preco_maco_cigarro > 0:
+            #             ( anos X 365 = dias )
+            total_gasto = anos_fumando * 365 * macos_por_dia * preco_maco_cigarro
+
+            if total_gasto < 20000:
+                print(f"Com o valor R$ {total_gasto:.2f}, Vossa Mercê poderia ter dado entrada em um carro.")
+
+            elif total_gasto < 50000:
+                print(f"Com o valor R$ {total_gasto:.2f}, Vossa Mercê poderia ter comprado um carro popular usado")
+
+            else:
+                print(f"Com o valor R$ {total_gasto:.2f}, Vossa Mercê poderia ter comprado um carro zero.")
+
+        elif preco_maco_cigarro == 0:
+            print("Os cigarros que Vossa Mercê fuma são gratuitos, então não gastou nada com cigarros.")
+
+        else:
+            print("Vossa Mercê digitou um valor inválido.")
+
+    elif macos_por_dia == 0:
+        print("Vossa mercê não fuma, então não gastou nada com cigarros.")
+
+    else:
+        print("Vossa Mercê digitou um valor inválido.")
+
+elif anos_fumando == 0:
+    print("Vossa Mercê não fuma, então não gastou nada com cigarros.")
+
 else:
-    print(f"Com o valor R$ {total_gasto:.2f}, você poderia ter comprado um carro zero.")
+    print("Vossa Mercê digitou um valor inválido.")
